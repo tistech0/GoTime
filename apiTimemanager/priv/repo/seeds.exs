60b336eeed782747e6fd75e0c7323ebcf7abdb05
tistech <contact@matthislesur.com>
2023-10-26 11:01:59 +0200

    🔧 creation user default

--- apiTimemanager/priv/repo/seeds.exs
+++ apiTimemanager/priv/repo/seeds.exs
@@ -50,3 +50,43 @@ Enum.each(roles_list, fn role ->
       IO.puts("Role '#{role.role}' already exists.")
   end
 end)
+
+# List of users to be inserted
+users_list = [
+  %{
+    username: "admin",
+    email: "admin@email.com",
+    password: "admin",
+    time_contract: 40,
+    role_id: 1
+  },
+  %{
+    username: "user",
+    email: "user@email.com",
+    password: "user",
+    time_contract: 35,
+    role_id: 2
+  },
+  %{
+    username: "superadmin",
+    email: "superadmin@email.com",
+    password: "superadmin",
+    time_contract: 40,
+    role_id: 3
+  }
+]
+
+Enum.each(users_list, fn user ->
+  # Check if the user already exists in the database
+  existing_user = Repo.get_by(Timemanager.Account.User, user)
+
+  case existing_user do
+    nil ->
+      # If the user doesn't exist, create it
+      user_changeset = Timemanager.Account.User.changeset(%Timemanager.Account.User{}, user)
+      Repo.insert!(user_changeset)
+    _ ->
+      # User already exists, do nothing
+      IO.puts("User '#{user.username}' already exists.")
+  end
+end)
