a4ee20f80febc91ecea4d4f6901d3e16d6c2082d
Rogy Podgy <julien.deshayes@epitech.eu>
2023-11-03 12:15:20 +0100

    :construction: register viex responsive

--- clientTimeManager/src/views/RegisterView.vue
+++ clientTimeManager/src/views/RegisterView.vue
@@ -10,20 +10,18 @@ const { mobile } = useDisplay()
 
 </script>
 <template>
-  <div class="about">
     <BottomNav v-if="mobile" />
     <SideBar v-else />
-    <v-main class="w-full h-full grid grid-cols-5 grid-flow-row-dense">
+    <v-main class="w-full h-full grid grid-cols-5 grid-flow-row-dense register-view">
       <div v-if="!mobile" class="col-span-5">
         <img src="../assets/profileBackgroung.png" alt="company" class="w-full" />
       </div>
-    <RegisterForm id="register-form" :class="!mobile ? 'col-span-3' : 'col-span-5'"/>
+      <RegisterForm id="register-form" :class="!mobile ? 'col-span-3 col-start-2' : 'col-span-5'"/>
     </v-main>
-  </div>
 </template>
 
 <style>
-#register-form {
+.register-view {
+    margin-bottom: 2rem;
 }
-
 </style>
