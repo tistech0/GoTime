ed3ccc2540558a22950050b416e9315bdb921c6c
bapti <baptiste.pegne@etu.univ-nantes.fr>
2023-11-03 17:25:53 +0100

    :construction: Add DeleteLogoutOverlay.vue

---
+++ clientTimeManager/src/components/DeleteLogoutOverlay.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Button from "../components/form/Button.vue";
+import {ref} from "vue";
+
+const props = defineProps({title: String, description: String});
+const showPopup = ref(false);
+
+
+const openPopup = () => {
+  showPopup.value = true;
+};
+
+const closePopup = () => {
+  showPopup.value = false;
+};
+
+defineExpose({openPopup, closePopup});
+</script>
+
+<template>
+  <div class="fixed inset-0 flex items-center justify-center z-50" v-if="showPopup">
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="bg-white p-6 rounded-lg shadow-lg z-10">
+      <v-icon>mdi-window-close</v-icon>
+      <h1 class="text-black font-semibold m-7">{{ props.title }}</h1>
+      <p class="font-semibold m-4">{{ props.description }}</p>
+      <Button buttonName="Validate" @on-click="closePopup"/>
+    </div>
+  </div>
+</template>
+
+
+
--- clientTimeManager/src/components/form/Button.vue
+++ clientTimeManager/src/components/form/Button.vue
@@ -1,29 +1,35 @@
 <script setup lang="ts">
-import { useDisplay } from 'vuetify';
+import {useDisplay} from 'vuetify';
+
 const props = defineProps<{ buttonName: string }>()
 const emit = defineEmits(["onClick"]);
 
-const { mobile } = useDisplay()
+const {mobile} = useDisplay()
 
 </script>
 
 <template>
-    <div class="button">
-        <v-btn :class="mobile ? 'full-width-button' : 'small-width-button' " color="var(--primary-blue)" @click="$emit('onClick')">
-            {{ props.buttonName }}
-        </v-btn>
-    </div>
+  <div class="button">
+    <v-btn :class="mobile ? 'full-width-button' : 'small-width-button' "
+           color="var(--primary-blue)"
+           class="text-white"
+           @click="$emit('onClick')">
+      {{ props.buttonName }}
+    </v-btn>
+  </div>
 </template>
 
 <style scoped>
 .button {
-    display: flex;
-    justify-content:center;
+  display: flex;
+  justify-content: center;
 }
+
 .small-width-button {
-    width: 50%;
+  width: 50%;
 }
+
 .full-width-button {
-    width: 100%;
+  width: 100%;
 }
 </style>
