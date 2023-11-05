a027f1277f7529bec5a898fc3c63f30892a3bc32
Rogy Podgy <julien.deshayes@epitech.eu>
2023-11-05 19:07:45 +0100

    :memo: Add documentation

--- clientTimeManager/src/stores/user.ts
+++ clientTimeManager/src/stores/user.ts
@@ -1,5 +1,8 @@
 import { defineStore } from 'pinia'
 
+/**
+ * This defines the pinia store for the current connected user. Its all the values stored and the getters to get specific ones.
+ */
 export const useUserStore = defineStore('user', {
   state: () => ({id: 0, username: '', email: '', role: '', time_contract: 0.0}),
   getters: {
