f4ff62922afff82b0d9d3d9323083a6926e5a796
tistech <contact@matthislesur.com>
2023-11-06 12:06:08 +0100

    :heavy_plus_sign: add apex chart js

--- clientTimeManager/src/main.ts
+++ clientTimeManager/src/main.ts
@@ -14,6 +14,9 @@ import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// ApexCharts
+import VueApexCharts from "vue3-apexcharts";
+
 const app = createApp(App);
 
 const vuetify = createVuetify({
@@ -41,5 +44,6 @@ const vuetify = createVuetify({
 app.use(createPinia());
 app.use(router);
 app.use(vuetify);
+app.use(VueApexCharts);
 
 app.mount("#app");
