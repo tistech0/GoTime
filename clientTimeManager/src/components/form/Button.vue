9d2b41b1ff62fd0f6d971f23364b07ad651ed7ab
tistech <contact@matthislesur.com>
2023-11-07 01:06:47 +0100

    :lipstick: add lisa button modif

--- clientTimeManager/src/components/form/Button.vue
+++ clientTimeManager/src/components/form/Button.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { useDisplay } from 'vuetify';
-const props = defineProps<{ buttonName: string }>()
+
+const props = defineProps<{ buttonName: string, btnColor: string }>()
+
 const emit = defineEmits(["onClick"]);
 
 const { mobile } = useDisplay()
@@ -8,22 +10,25 @@ const { mobile } = useDisplay()
 </script>
 
 <template>
-    <div class="button">
-        <v-btn :class="mobile ? 'full-width-button' : 'small-width-button' " color="var(--primary-blue)" @click="$emit('onClick')">
-            {{ props.buttonName }}
-        </v-btn>
-    </div>
+  <div class="button">
+    <v-btn :class="mobile ? 'full-width-button' : 'small-width-button'"
+           :color="btnColor == 'blue' ? 'var(--primary-blue)' : 'var(--secondary-pink)'" @click="$emit('onClick')">
+      {{ props.buttonName }}
+    </v-btn>
+  </div>
 </template>
 
 <style scoped>
 .button {
-    display: flex;
-    justify-content:center;
+  display: flex;
+  justify-content: center;
 }
+
 .small-width-button {
-    @apply w-1/2
+  @apply w-1/2
 }
+
 .full-width-button {
-    @apply w-full
+  @apply w-full
 }
 </style>
