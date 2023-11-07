910d0d61dc0b15fd63c2d77f652ddf6c484e4a9d
tistech <contact@matthislesur.com>
2023-11-07 01:07:09 +0100

    :lipstick: handle emit week selector

--- clientTimeManager/src/views/DashboardView.vue
+++ clientTimeManager/src/views/DashboardView.vue
@@ -1,75 +1,50 @@
 <script setup lang="ts">
-import Timer from '../components/Timer.vue';
-import BottomNav from '@/components/BottomNav.vue';
-import Sidebar from '@/components/SideBar.vue';
-import ManagerGraph from '@/components/ManagerGraph.vue';
 import { useDisplay } from 'vuetify';
-
-
+import WeekSelector from "@/components/WeekSelector.vue";
 const { mobile } = useDisplay()
-
-let data = [
-        {
-            "id": 1,
-            "start": "2023-10-25T20:48:13",
-            "end": "2023-10-25T20:48:37",
-            "valueDay": 3,
-            "valueNight": 0,
-        },
-        {
-            "id": 2,
-            "start": "2023-11-25T09:23:45",
-            "end": "2023-11-25T18:30:31",
-            "valueDay": 4,
-            "valueNight": 3,
-        },
-        {
-            "id": 3,
-            "start": "2023-12-25T10:21:25",
-            "end": "2023-12-25T17:49:56",
-            "valueDay": 3,
-            "valueNight": 8,
-        }
-    ]
-
-let min = [1, 2, 3, 2, 4, 3];
-let max = [6, 7, 8, 4, 7, 6];
-let series = createSeries(data);
-
-function createSeries(data: any) {
-  let series = [];
-  let day: any = [];
-  let night: any = [];
-
-  data.forEach((element: any) => {
-    day.push(element.valueDay);
-    night.push(element.valueNight);
-  });
-
-  series.push({
-    name: "Day average time",
-    data: day,
-    color: "#48A9A6",
-  });
-
-  series.push({
-    name: "Nigth average time",
-    data: night,
-    color: "#1B2A41",
-  });
-
-  return series;
-}
-
 </script>
 
 <template>
   <BottomNav v-if="mobile" />
   <Sidebar v-else />
   <v-main class="w-full h-full grid grid-cols-5 grid-flow-row-dense">
-    <Timer class=" col-span-2" />
-    <ManagerGraph :min="min" :max="max" :series="series" class="col-span-3" />
+    <Timer class=" col-span-2"/>
+    <div class="data-wrapper col-span-3">
+      <WeekSelector @week-updated="updateWeek"/>
+      <TimeGraph :start="start" :end="end" :key="`${start}-${end}`"/>
+    </div>
   </v-main>
 </template>
+
+<script lang="ts">
+import BottomNav from '../components/BottomNav.vue';
+import Sidebar from '../components/SideBar.vue';
+import Timer from '../components/Timer.vue';
+import TimeGraph from '../components/TimeGraph.vue';
+
+export default {
+  components: { Timer, BottomNav, Sidebar, TimeGraph },
+  data() {
+    return {
+      start: new Date(),
+      end: this.initOneWeekAgo(),
+    };
+  },
+  methods : {
+    initOneWeekAgo() {
+      let week = new Date();
+      week.setDate(week.getDate() - 6);
+      return week;
+    },
+    updateWeek(currentDate: Date) {
+      this.start = new Date(currentDate);
+      this.end = new Date(currentDate);
+      this.end.setDate(this.end.getDate() - 6);
+      console.log("start: start", this.start, "end: ", this.end);
+    }
+  }
+};
+
+</script>
 
 <style scoped></style>
