09b6235cc0a9046f6c6028bba2fcd2beedd6a5b3
tistech <contact@matthislesur.com>
2023-11-07 15:25:17 +0100

    :wrench: set proxy in the vite config file

--- clientTimeManager/vite.config.ts
+++ clientTimeManager/vite.config.ts
@@ -1,19 +1,24 @@
-import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import VueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [VueDevTools(), vue()],
   server: {
     port: 8081,
     host: "0.0.0.0",
+    proxy: {
+      '/api': {
+        target: process.env.NODE_ENV === 'production' ? `http://${process.env.VUE_APP_API_HOST}:4000` : 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
-  plugins: [VueDevTools(), vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      '@': '/src'
+    }
+  }
+})
