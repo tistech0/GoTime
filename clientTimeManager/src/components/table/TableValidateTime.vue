4bb3042ae72f6ee8002f0f4eb1c16e22cc99c979
LisaBourdon <lisa.bourdon@epitech.eu>
2023-11-08 10:04:51 +0100

    :construction: fetch stat table in validate time with interface type

--- clientTimeManager/src/components/table/TableValidateTime.vue
+++ clientTimeManager/src/components/table/TableValidateTime.vue
@@ -1,107 +1,47 @@
 <script setup lang="ts">
 
-const stat = [
-    {
-        id: 1,
-        username: "HGefbolij",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 0.0,
-        valueNight: 9.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
+import { ref } from 'vue';
+import type { TableStats } from '../../types/tableStats.ts';
 
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
-    {
-        id: 2,
-        username: "Ijifhqenwsdk",
-        start: "2023-10-25T20:48:13",
-        status: "validated",
-        end: "2023-10-26T05:46:13",
-        valueDay: 4.0,
-        valueNight: 3.0
-    },
 
-]
+let workingTimesList = ref<TableStats[]>([]);
 
-// calculate sum of valueDay and valueNight for each user
 
+const fetchData = async () => {
+    try {
+        const response = await fetch('http://localhost:4000/api/stats/team/workingtimes/all/1?start=2022-10-25 20:48:13&end=2025-10-25 20:48:37',
+            {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        const data = await response.json();
+
+        const waitingWorkingTimes = data.data.filter((item: { status: string }) => item.status === 'waiting');
+
+        workingTimesList.value = waitingWorkingTimes.map(
+            (w: any) => ({ // TODO: create interface to replace any
+                start: new Date(w.start),
+                end: new Date(w.end),
+                valueDay: parseInt(w.valueDay),
+                valueNight: parseInt(w.valueNight),
+            })
+        );
+
+        console.log(workingTimesList.value);
+    } catch (error) {
+        console.error(error);
+
+    }
+};
+fetchData();
 
 </script>
 
 <template>
-    <v-table class="col-span-3 col-start-2" fixed-header height="500px">
+    <v-table class="col-span-3 col-start-2 mt-10" fixed-header height="500px">
         <thead class="drop-shadow-md">
             <tr>
                 <th class="text-left">
@@ -128,28 +68,60 @@ const stat = [
             </tr>
         </thead>
         <tbody>
-            <tr v-for="item in stat" :key="item.id">
-                <td>{{ item.username }}</td>
-                <td>{{ item.start }}</td>
-                <td>{{ item.end }}</td>
-                <td>{{ item.valueDay }}</td>
-                <td>{{ item.valueNight }}</td>
-                <td>
-                    <!-- sum valueDay and valueNight -->
-                    {{
-                        item.valueDay + item.valueNight
+            <template v-if="workingTimesList.length === 0">
+                <tr>
+                    <td colspan="7" class="text-center">
+                        No data available
+                    </td>
+                </tr>
+            </template>
+            <template v-else>
+                <tr v-for="item in workingTimesList" :key="item.id">
+                    <td>
+                        <!-- {{ item.user.username }} -->
+                        MACHIN CHOSE
+                    </td>
+                    <td>
+                        {{
+                            item.start.toLocaleString("en", {
+                                month: "short",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "numeric",
+                                year: "numeric",
+                            })
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            item.end.toLocaleString("en", {
+                                month: "short",
+                                day: "numeric",
+                                hour: "numeric",
+                                minute: "numeric",
+                                year: "numeric",
+                            })
+                        }}
+                    </td>
+                    <td>{{ (Math.round(item.valueDay * 100) / 100).toFixed(2) }} h</td>
+                    <td>{{ (Math.round(item.valueNight * 100) / 100).toFixed(2) }} h</td>
+                    <td>
+                        <!-- sum valueDay and valueNight -->
+                        {{
+                            (Math.round((item.valueDay + item.valueNight) * 100) / 100).toFixed(2)
 
-                    }}
-                </td>
-                <td>
-                    <v-icon class="mr-2">
-                        mdi-check
-                    </v-icon>
-                    <v-icon class="mr-2">
-                        mdi-close
-                    </v-icon>
-                </td>
-            </tr>
+                        }}
+                    </td>
+                    <td>
+                        <v-icon class="mr-2">
+                            mdi-check
+                        </v-icon>
+                        <v-icon class="mr-2">
+                            mdi-close
+                        </v-icon>
+                    </td>
+                </tr>
+            </template>
         </tbody>
     </v-table>
 </template>
--- clientTimeManager/src/views/ValidateTimeView.vue
+++ clientTimeManager/src/views/ValidateTimeView.vue
@@ -5,23 +5,7 @@ import { useDisplay } from 'vuetify';
 import TableValidateTime from '../components/table/TableValidateTime.vue';
 import Hello from '../components/Hello.vue';
 
-
 const { mobile } = useDisplay()
-
-
-async function getUser() {
-    const response = await fetch("http://localhost:4000/api/user", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    const data = await response.json();
-    console.log(data);
-}
-
-getUser();
 </script>
 
 <template>
