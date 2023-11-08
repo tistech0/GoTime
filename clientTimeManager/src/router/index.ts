88497332a30ca66d0ea6b47951e411412d4cf763
tistech <contact@matthislesur.com>
2023-11-08 16:31:43 +0100

    :wrench: fix merge change

--- clientTimeManager/src/router/index.ts
+++ clientTimeManager/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DashboardView from "../views/DashboardView.vue";
 import LoginView from "../views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 import ProfileView from "../views/ProfileView.vue";
 import ValidateTimeView from "../views/ValidateTimeView.vue";
 import EditProfileView from "../views/EditProfileView.vue";
