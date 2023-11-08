7a6135b22f0e9e898d5f06ca1efd2692bf01b889
tistech <contact@matthislesur.com>
2023-11-08 16:35:04 +0100

    :lipstick: add DashboardView.vue

--- clientTimeManager/src/views/DashboardView.vue
+++ clientTimeManager/src/views/DashboardView.vue
@@ -1,39 +1,8 @@
 <script lang="ts" setup>
 import {useDisplay} from 'vuetify';
 import WeekSelector from "@/components/WeekSelector.vue";
-import type { TableStats } from '../types/tableStats';
-import { ref } from "vue";
 
 const {mobile} = useDisplay()
-
-let workingTimesList = ref<TableStats[]>([]);
-const fetchData = async () => {
-  try {
-    const apiUrl = import.meta.env.VITE_API_URL;
-    const response = await fetch(`${apiUrl}/api/workingtimes/1?start=2023-10-25 20:48:13&end=2023-12-25 17:49:56`,
-        {
-          method: "GET",
-          credentials: "include",
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-    const data = await response.json();
-    const waitingWorkingTimes = data.data.filter((item: { status: string }) => item.status === 'waiting');
-    workingTimesList.value = waitingWorkingTimes.map(
-        (w: any) => ({ // TODO: create interface to replace any
-          start: new Date(w.start),
-          end: new Date(w.end),
-          valueDay: parseInt(w.valueDay),
-          valueNight: parseInt(w.valueNight),
-        })
-    );
-    console.log(workingTimesList.value);
-  } catch (error) {
-    console.error(error);
-  }
-};
-fetchData();
 </script>
 
 <template>
@@ -43,9 +12,9 @@ fetchData();
     <Timer class="md:col-span-2"/>
     <div class="data-wrapper md:col-span-3">
       <WeekSelector @week-updated="updateWeek"/>
-      <TimeGraph :key="`${start}-${end}`" :end="end" :start="start"/>
-      <hr>
-      <v-table class="col-span-3 col-start-2 mt-10" fixed-header height="500px">
+      <TimeGraph :key="`${start}-${end}`" :end="end" :start="start" :workingTimeList="workingTimesList"/>
+      <hr v-if="workingTimesList.length > 0">
+      <v-table class="" fixed-header>
         <thead class="drop-shadow-md">
         <tr>
           <th class="text-left">
@@ -100,18 +69,11 @@ fetchData();
             <td>{{ (Math.round(item.valueDay * 100) / 100).toFixed(2) }} h</td>
             <td>{{ (Math.round(item.valueNight * 100) / 100).toFixed(2) }} h</td>
             <td>
-              <!-- sum valueDay and valueNight -->
-              {{
-                (Math.round((item.valueDay + item.valueNight) * 100) / 100).toFixed(2)
-
-              }}
-            </td>
-            <td>
               <v-icon class="mr-2">
-                mdi-check
+                mdi-clock-edit-outline
               </v-icon>
               <v-icon class="mr-2">
-                mdi-close
+                mdi-delete-alert-outline
               </v-icon>
             </td>
           </tr>
@@ -123,10 +85,12 @@ fetchData();
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import BottomNav from '../components/BottomNav.vue';
 import Sidebar from '../components/SideBar.vue';
 import Timer from '../components/Timer.vue';
-import TimeGraph from '../components/TimeGraph.vue';
+import TimeGraph from '../components/TimeGraphUser.vue';
+import type { TableStats } from '../types/tableStats';
 
 export default {
   components: {Timer, BottomNav, Sidebar, TimeGraph},
@@ -134,6 +98,7 @@ export default {
     return {
       start: new Date(),
       end: this.initOneWeekAgo(),
+      workingTimesList: ref<TableStats[]>([])
     };
   },
   methods: {
@@ -142,14 +107,54 @@ export default {
       week.setDate(week.getDate() - 6);
       return week;
     },
-    updateWeek(currentDate: Date) {
+    async updateWeek(currentDate: Date) {
       this.start = new Date(currentDate);
       this.end = new Date(currentDate);
       this.end.setDate(this.end.getDate() - 6);
+      await this.fetchData();
+    },
+    async fetchData() {
+      this.workingTimesList = [];
+      try {
+        const apiUrl = import.meta.env.VITE_API_URL;
+        const startTime = this.formatDate(this.end);
+        const endTime = this.formatDate(this.start)
+        const response = await fetch(`${apiUrl}/api/workingtimes/172cae9c-248a-4986-baae-e8f651067ccb?start=${startTime}&end=${endTime}`,
+            {
+              method: "GET",
+              credentials: "include",
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            });
+        const data = await response.json();
+        const validateWorkingTimes = data.data.filter((item: { status: string }) => item.status === 'validated');
+        this.workingTimesList = validateWorkingTimes.map(
+            (w: any) => ({ // TODO: create interface to replace any
+              start: new Date(w.start),
+              end: new Date(w.end),
+              valueDay: parseFloat(w.valueDay),
+              valueNight: w.valueNight ? parseFloat(w.valueNight) : 0,
+            })
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    formatDate(date: Date) {
+      const year = date.getFullYear();
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
+      const day = ('0' + date.getDate()).slice(-2);
+      const hours = ('0' + date.getHours()).slice(-2);
+      const minutes = ('0' + date.getMinutes()).slice(-2);
+      const seconds = ('0' + date.getSeconds()).slice(-2);
+      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     }
+  },
+  mounted() {
+    this.fetchData();
   }
 };
-
 </script>
 
 <style scoped>
@@ -162,6 +167,11 @@ hr {
   width: 70%;
   margin-left: auto;
   margin-right: auto;
+}
+
+.data-wrapper {
+  width: 80%;
+  margin: 50px auto;
 }
 
 </style>
