603ec1734aac1fa3c6d9f08ae87731aa97b66cb2
baptiste <baptiste.pegne@epitech.eu>
2023-11-08 10:27:14 +0100

    :iphone: DeleteLogoutOverlay.vue

--- clientTimeManager/src/components/overlay/DeleteLogoutOverlay.vue
+++ clientTimeManager/src/components/overlay/DeleteLogoutOverlay.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Button from "../form/Button.vue";
 import {ref} from "vue";
+import {useDisplay} from "vuetify";
 
 const props = defineProps({title: String, description: String});
 const showPopup = ref(false);
+const { mobile } = useDisplay()
 
 
 const openPopup = () => {
@@ -18,20 +20,27 @@ defineExpose({openPopup, closePopup});
 </script>
 
 <template>
-  <div class="fixed inset-0 flex items-center justify-center z-50" v-if="showPopup">
+  <div class="fixed inset-0 flex items-center justify-center z-2" v-if="showPopup">
     <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div class="bg-white p-6 rounded-lg shadow-lg z-10">
-      <div class="flex justify-between items-start">
-        <div>
-          <h1 class="text-black font-semibold m-7">{{ props.title }}</h1>
-          <p class="font-semibold m-4">{{ props.description }}</p>
+    <div :class="{'bg-white p-6 rounded-lg shadow-lg z-10': true, 'mobile-class': mobile}">
+      <div class="grid grid-cols-2 items-start">
+        <v-icon class="mt-2 col-start-2 justify-self-end" @click="closePopup">mdi-window-close</v-icon>
+        <div class="col-span-2">
+          <h1 class="text-black font-semibold">{{ props.title }}</h1>
+          <p class="font-semibold m-4 text-center">{{ props.description }}</p>
         </div>
-        <v-icon class="mt-2" @click="closePopup">mdi-window-close</v-icon>
       </div>
-      <Button buttonName="Validate" @on-click="closePopup"/>
+      <Button buttonName="Validate" btn-color="blue" @on-click="closePopup"/>
     </div>
   </div>
 </template>
+
+<style>
+.mobile-class {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+</style>
 
 
 
