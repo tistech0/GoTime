4d1fb3ad113a4bc64e9e07123ee8e9eb117656f2
tistech <contact@matthislesur.com>
2023-11-09 12:16:09 +0100

    :wrench: add endpoint deployement

--- apiTimemanager/lib/timemanager_web/endpoint.ex
+++ apiTimemanager/lib/timemanager_web/endpoint.ex
@@ -52,7 +52,7 @@ defmodule TimemanagerWeb.Endpoint do
   plug(CORSPlug,
     origin: [
       "http://localhost:8081",
-      "http://34.155.133.157"
+      "http://34.163.234.235"
     ],
     allow_headers: ["content-type", "authorization"],
     allow_methods: ["GET", "POST", "PUT", "DELETE"],
