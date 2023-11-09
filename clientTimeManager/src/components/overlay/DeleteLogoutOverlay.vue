bb2c0366999929b9d21f9ef0f2e3d8d0b725c3f2
LisaBourdon <lisa.bourdon@epitech.eu>
2023-11-09 15:38:49 +0100

    :lipstick: add logout overlay sidebar

--- clientTimeManager/src/components/overlay/DeleteLogoutOverlay.vue
+++ clientTimeManager/src/components/overlay/DeleteLogoutOverlay.vue
@@ -1,36 +1,50 @@
 <script setup lang="ts">
 import Button from "../form/Button.vue";
-import {ref} from "vue";
-import {useDisplay} from "vuetify";
+import { ref } from "vue";
+import { useDisplay } from "vuetify";
 
-const props = defineProps({title: String, description: String});
-const showPopup = ref(false);
-const { mobile } = useDisplay()
+const props = defineProps<{
+  title: string;
+  description: string;
+  visible: boolean;
+  onAction: () => void;
+}>();
 
+defineEmits(["update:visible"]);
 
-const openPopup = () => {
-  showPopup.value = true;
-};
-
-const closePopup = () => {
-  showPopup.value = false;
-};
-
-defineExpose({openPopup, closePopup});
+const { mobile } = useDisplay();
 </script>
 
 <template>
-  <div class="fixed inset-0 flex items-center justify-center z-2" v-if="showPopup">
+  <div class="fixed inset-0 flex items-center justify-center z-2">
     <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div :class="{'bg-white p-6 rounded-lg shadow-lg z-10': true, 'mobile-class': mobile}">
+    <div
+      :class="{
+        'bg-white p-6 rounded-lg shadow-lg z-10': true,
+        'mobile-class': mobile,
+      }"
+    >
       <div class="grid grid-cols-2 items-start">
-        <v-icon class="mt-2 col-start-2 justify-self-end" @click="closePopup">mdi-window-close</v-icon>
+        <v-icon
+          class="mt-2 col-start-2 justify-self-end"
+          @click="$emit('update:visible', false)"
+          >mdi-window-close</v-icon
+        >
         <div class="col-span-2">
           <h1 class="text-black font-semibold">{{ props.title }}</h1>
           <p class="font-semibold m-4 text-center">{{ props.description }}</p>
         </div>
       </div>
-      <Button buttonName="Validate" btn-color="blue" @on-click="closePopup"/>
+      <Button
+        buttonName="Validate"
+        btn-color="blue"
+        @click="
+          () => {
+            $emit('update:visible', false);
+            props.onAction();
+          }
+        "
+      />
     </div>
   </div>
 </template>
@@ -41,6 +55,3 @@ defineExpose({openPopup, closePopup});
   margin-right: 10px;
 }
 </style>
-
-
-
