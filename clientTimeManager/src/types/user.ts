99e3a3e359cebd24f1ff60dd42c10a80702a83f7
LisaBourdon <lisa.bourdon@epitech.eu>
2023-11-11 00:23:37 +0100

    ♻️ Refactor user interface in clientTimeManager

--- clientTimeManager/src/types/user.ts
+++ clientTimeManager/src/types/user.ts
@@ -2,10 +2,9 @@
  * This interface represent the user data stored in pinia
  */
 export interface User {
-    id: string, 
-    username: string, 
-    email: string,
-    role: string,
-    time_contract: number,
+  id: string;
+  username: string;
+  email: string;
+  role: "User" | "Admin" | "SuperAdmin";
+  time_contract: number;
 }
-  
