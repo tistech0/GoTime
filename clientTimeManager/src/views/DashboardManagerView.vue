82808296e9a4c2fea4df24ac4d615950c29e1d2d
LisaBourdon <lisa.bourdon@epitech.eu>
2023-11-11 00:25:35 +0100

    ✨ add delete time Dashboard Manager

--- clientTimeManager/src/views/DashboardManagerView.vue
+++ clientTimeManager/src/views/DashboardManagerView.vue
@@ -1,109 +1,121 @@
 <script lang="ts" setup>
-import {useDisplay} from 'vuetify';
-import TimeGraphManger from '@/components/TimeGraphManager.vue';
+import { useDisplay } from "vuetify";
+import TimeGraphManger from "@/components/TimeGraphManager.vue";
 import WeekSelector from "@/components/WeekSelector.vue";
 import SelectOne from "@/components/form/SelectOne.vue";
-import BottomNav from '@/components/BottomNav.vue';
-import Sidebar from '@/components/SideBar.vue';
+import BottomNav from "@/components/BottomNav.vue";
+import Sidebar from "@/components/SideBar.vue";
 import TimeGraphManager from "@/components/TimeGraphManager.vue";
+import Button from "../components/form/Button.vue";
 
-const { mobile } = useDisplay()
+const { mobile } = useDisplay();
+const deleteTeamPopupVisible = ref(false);
 
 // TODO: a manager can only see the working times of his team
-
-
 </script>
 
 <template>
-  <BottomNav v-if="mobile"/>
-  <Sidebar v-else/>
+  <BottomNav v-if="mobile" />
+  <Sidebar v-else />
+  <DeleteLogoutOverlay
+    ref="deleteTimePopup"
+    title="Delete your time"
+    description="Are you sure you want to delete this time?"
+    @action="deleteTeam(queryUuid)"
+    v-model:visible="deleteTeamPopupVisible"
+    v-if="deleteTeamPopupVisible"
+  />
   <v-main class="w-full h-full grid grid-cols-1">
+    <!-- TODO: Delete this button -->
+    <Button
+      btnColor="pink"
+      buttonName="Delete Team"
+      @click="deleteTeamPopupVisible = true"
+    ></Button>
     <div class="selector-wrapper">
-      <SelectOne class="w-60 text-xs col-start-2 mt-20" label="Pick team" :itemList=listTeam hint="Pick a team"
-                 v-model="queryUuid" @update:modelValue="fetchTeam()" :clearable=false />
-      <WeekSelector @week-updated="updateWeek"/>
+      <SelectOne
+        class="w-60 text-xs col-start-2 mt-20"
+        label="Pick team"
+        :itemList="listTeam"
+        hint="Pick a team"
+        v-model="queryUuid"
+        @update:modelValue="fetchTeam()"
+        :clearable="false"
+      />
+      <WeekSelector @week-updated="updateWeek" />
     </div>
-    <TimeGraphManager v-if="workingTimesListTeam.length > 0" :key="`${start}-${end}`" :end="end" :start="start" :workingTimeList="workingTimesListTeam"/>
+    <TimeGraphManager
+      v-if="workingTimesListTeam.length > 0"
+      :key="`${start}-${end}`"
+      :end="end"
+      :start="start"
+      :workingTimeList="workingTimesListTeam"
+    />
     <h2 v-else class="no-data">No data you need to pick a team</h2>
-    <hr v-if="workingTimesList.length > 0">
+    <hr v-if="workingTimesList.length > 0" />
     <v-table class="" fixed-header>
       <thead class="drop-shadow-md">
-      <tr>
-        <th class="text-left">
-          Start
-        </th>
-        <th class="text-left">
-          End
-        </th>
-        <th class="text-left">
-          Day
-        </th>
-        <th class="text-left">
-          Night
-        </th>
-        <th class="text-left">
-          Operation
-        </th>
-      </tr>
+        <tr>
+          <th class="text-left">Start</th>
+          <th class="text-left">End</th>
+          <th class="text-left">Day</th>
+          <th class="text-left">Night</th>
+          <th class="text-left">Operation</th>
+        </tr>
       </thead>
       <tbody>
-      <template v-if="workingTimesList.length === 0">
-        <tr>
-          <td class="text-center" colspan="7">
-            No data available
-          </td>
-        </tr>
-      </template>
-      <template v-else>
-        <tr v-for="item in workingTimesList" :key="item.id">
-          <td>
-            {{
-              item.start.toLocaleString("en", {
-                month: "short",
-                day: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-                year: "numeric",
-              })
-            }}
-          </td>
-          <td>
-            {{
-              item.end.toLocaleString("en", {
-                month: "short",
-                day: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-                year: "numeric",
-              })
-            }}
-          </td>
-          <td>{{ formatHourMin(item.valueDay) }}</td>
-          <td>{{ formatHourMin(item.valueNight) }}</td>
-          <td>
-            <v-icon class="mr-2">
-              mdi-clock-edit-outline
-            </v-icon>
-            <v-icon class="mr-2">
-              mdi-delete-alert-outline
-            </v-icon>
-          </td>
-        </tr>
-      </template>
+        <template v-if="workingTimesList.length === 0">
+          <tr>
+            <td class="text-center" colspan="7">No data available</td>
+          </tr>
+        </template>
+        <template v-else>
+          <tr v-for="item in workingTimesList" :key="item.id">
+            <td>
+              {{
+                item.start.toLocaleString("en", {
+                  month: "short",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                  year: "numeric",
+                })
+              }}
+            </td>
+            <td>
+              {{
+                item.end.toLocaleString("en", {
+                  month: "short",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                  year: "numeric",
+                })
+              }}
+            </td>
+            <td>{{ formatHourMin(item.valueDay) }}</td>
+            <td>{{ formatHourMin(item.valueNight) }}</td>
+            <td>
+              <v-icon class="mr-2"> mdi-clock-edit-outline </v-icon>
+              <v-icon class="mr-2"> mdi-delete-alert-outline </v-icon>
+            </td>
+          </tr>
+        </template>
       </tbody>
     </v-table>
   </v-main>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 import { useUserStore } from "@/stores/user";
-import {useSnackbarStore} from "@/stores/snackbar";
-import {useRouter} from "vue-router";
-import {errorHandling} from "@/utils/utils";
-import type {TableStats} from "@/types/tableStats";
-import type {TeamStats} from "@/types/teamStats";
-import type {Item} from "@/types/items";
+import { useSnackbarStore } from "@/stores/snackbar";
+import { useRouter } from "vue-router";
+import { errorHandling } from "@/utils/utils";
+import DeleteLogoutOverlay from "../components/overlay/DeleteLogoutOverlay.vue";
+import type { TableStats } from "@/types/tableStats";
+import type { TeamStats } from "@/types/teamStats";
+import type { Item } from "@/types/items";
 
 export default {
   data() {
@@ -134,42 +146,64 @@ export default {
       this.end.setDate(this.end.getDate() - 6);
       await this.fetchData();
     },
+    async deleteTeam(uuid: string) {
+      try {
+        console.log("THIS IS A DELETE" + uuid);
+
+        const response = await fetch(`${this.apiUrl}/api/teams/${uuid}`, {
+          method: "DELETE",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (!response.ok) {
+          errorHandling(response, this.snackbarStore, this.router);
+          return;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
     async fetchData() {
       this.workingTimesList = [];
       try {
         const apiUrl = import.meta.env.VITE_API_URL;
         const startTime = this.formatDate(this.end);
-        const endTime = this.formatDate(this.start)
-        const response = await fetch(`${apiUrl}/api/workingtimes/${this.userId}?start=${startTime}&end=${endTime}`,
-            {
-              method: "GET",
-              credentials: "include",
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            });
+        const endTime = this.formatDate(this.start);
+        const response = await fetch(
+          `${apiUrl}/api/workingtimes/${this.userId}?start=${startTime}&end=${endTime}`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         const data = await response.json();
 
-        const validateWorkingTimes = data.data.filter((item: { status: string }) => item.status === 'validated');
-        this.workingTimesList = validateWorkingTimes.map(
-            (w: any) => ({ // TODO: create interface to replace any
-              start: new Date(w.start),
-              end: new Date(w.end),
-              valueDay: parseFloat(w.valueDay).toFixed(2),
-              valueNight: w.valueNight ? parseFloat(w.valueNight) : 0,
-            })
+        const validateWorkingTimes = data.data.filter(
+          (item: { status: string }) => item.status === "validated"
         );
+        this.workingTimesList = validateWorkingTimes.map((w: any) => ({
+          // TODO: create interface to replace any
+          start: new Date(w.start),
+          end: new Date(w.end),
+          valueDay: parseFloat(w.valueDay).toFixed(2),
+          valueNight: w.valueNight ? parseFloat(w.valueNight) : 0,
+        }));
       } catch (error) {
         console.error(error);
       }
     },
     formatDate(date: Date) {
       const year = date.getFullYear();
-      const month = ('0' + (date.getMonth() + 1)).slice(-2);
-      const day = ('0' + date.getDate()).slice(-2);
-      const hours = ('0' + date.getHours()).slice(-2);
-      const minutes = ('0' + date.getMinutes()).slice(-2);
-      const seconds = ('0' + date.getSeconds()).slice(-2);
+      const month = ("0" + (date.getMonth() + 1)).slice(-2);
+      const day = ("0" + date.getDate()).slice(-2);
+      const hours = ("0" + date.getHours()).slice(-2);
+      const minutes = ("0" + date.getMinutes()).slice(-2);
+      const seconds = ("0" + date.getSeconds()).slice(-2);
       return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     },
     formatHourMin(value: number) {
@@ -182,12 +216,12 @@ export default {
         method: "GET",
         credentials: "include",
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       });
       if (!response.ok) {
         errorHandling(response, this.snackbarStore, this.router);
-        return
+        return;
       }
       const data = await response.json();
       this.listTeam.values = data.data;
@@ -195,51 +229,55 @@ export default {
     async fetchTeam() {
       console.log("THIS IS A FETCH" + this.queryUuid);
       const startTime = this.formatDate(this.end);
-      const endTime = this.formatDate(this.start)
-      console.log(`${this.apiUrl}/api/stats/team/workingtimes/average/${this.queryUuid}?start=${startTime}&end=${endTime}`);
+      const endTime = this.formatDate(this.start);
+      console.log(
+        `${this.apiUrl}/api/stats/team/workingtimes/average/${this.queryUuid}?start=${startTime}&end=${endTime}`
+      );
       try {
-        const response = await fetch(`${this.apiUrl}/api/stats/team/workingtimes/average/${this.queryUuid}?start=${startTime}&end=${endTime}`,
-            {
-              method: "GET",
-              credentials: "include",
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            });
+        const response = await fetch(
+          `${this.apiUrl}/api/stats/team/workingtimes/average/${this.queryUuid}?start=${startTime}&end=${endTime}`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         const data = await response.json();
 
-        this.workingTimesListTeam = data.data.map(
-            (w: TeamStats) => ({ // TODO: create interface to replace any
-              ...w,
-              day: w.day,
-              start: new Date(w.start),
-              end: new Date(w.end),
-              average_day_hours: w.average_day_hours.toFixed(2),
-              average_night_hours: w.average_night_hours ? w.average_night_hours : 0,
-              average_hours: w.average_hours.toFixed(2),
-              total_day_hours: w.total_day_hours.toFixed(2),
-              total_night_hours: w.total_night_hours ? w.total_night_hours : 0,
-              total_hours: w.total_hours.toFixed(2),
-              max_hours: w.max_hours.toFixed(2),
-              min_hours: w.min_hours.toFixed(2),
-            })
-        );
+        this.workingTimesListTeam = data.data.map((w: TeamStats) => ({
+          // TODO: create interface to replace any
+          ...w,
+          day: w.day,
+          start: new Date(w.start),
+          end: new Date(w.end),
+          average_day_hours: w.average_day_hours.toFixed(2),
+          average_night_hours: w.average_night_hours
+            ? w.average_night_hours
+            : 0,
+          average_hours: w.average_hours.toFixed(2),
+          total_day_hours: w.total_day_hours.toFixed(2),
+          total_night_hours: w.total_night_hours ? w.total_night_hours : 0,
+          total_hours: w.total_hours.toFixed(2),
+          max_hours: w.max_hours.toFixed(2),
+          min_hours: w.min_hours.toFixed(2),
+        }));
 
         console.log(this.workingTimesListTeam);
       } catch (error) {
         console.error(error);
       }
-    }
+    },
   },
   async created() {
     await this.getTeamList();
     await this.fetchData();
-  }
+  },
 };
 </script>
 
 <style scoped>
-
 hr {
   border: 0;
   height: 1px;
@@ -263,5 +301,4 @@ hr {
   text-align: center;
   margin-top: 1rem;
 }
-
 </style>
